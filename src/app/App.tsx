f7cafe433c66b18fbaf38646aa918a93243ecb20
Michael Bradvica <mjbradvica@protonmail.com>
2021-03-20 16:41:38 -0700

    project setup

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../../node_modules/bulma/css/bulma.min.css';
+import './App.css';
+
+const App: React.FC = () => {
+	return <p>Hello!</p>;
+};
+
+export default App;
